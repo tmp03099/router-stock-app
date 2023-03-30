6a7abcb0f095573f3167726c0d826ae01f5b7794
My Phung Tieu <tmp03099@gmail.com>
2023-03-30 17:17:04 -0400

    updated

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,31 +1,28 @@
-import { useState, useEffect } from 'react'; 
-import { useParams } from 'react-router-dom';
-import { getPrices } from '../services/stock-api';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getPrices } from "../services/stock-api";
 
+function Stock() {
+  const [priceData, setPriceData] = useState([]);
 
+  //accesing the params obj to get the symbol params
+  const { symbol } = useParams();
+  console.log(symbol);
 
-function Stock(){
-    const [priceData, setPriceData] = useState([]);
+  //call the getStock function when the component render
+  useEffect(() => {
+    const loadPrice = async (symbol) => {
+      const data = await getPrices(symbol);
+      setPriceData(data);
+    };
+    loadPrice(symbol);
+  }, []);
 
-    //accesing the params obj to get the symbol params
-    const {symbol} = useParams();
-    console.log(symbol);
-
-    //call the getStock function when the component render
-    useEffect(() => {
-        const loadPrice = async() => {
-            const data = await getPrices(symbol);
-            setPriceData(data);
-        }
-        loadPrice();
-    },[]);
-
-    return(
-        <div>
-            <h3>Name: {priceData.symbol}</h3>
-            <h3>Price: {priceData.price}</h3>
-
-        </div>
-    )
+  return (
+    <div>
+      <h3>Name: {priceData.symbol}</h3>
+      <h3>Price: {priceData.price}</h3>
+    </div>
+  );
 }
 export default Stock;
