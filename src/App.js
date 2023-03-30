dddbc743770281e57850ea30c80d4717c4f6cd95
My Phung Tieu <tmp03099@gmail.com>
2023-03-30 17:06:38 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Stock from './pages/Stock';
-import Dashboard from './pages/Dashboard';
-import NavBar from './components/NavBar';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Stock from "./pages/Stock";
+import Dashboard from "./pages/Dashboard";
+import MyNav from "./components/MyNav";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <MyNav />
 
-      <NavBar/>
-      
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/stocks/:symbol" element={<Stock/>}/>
-        <Route path="/stocks" element={<Dashboard/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks/:symbol" element={<Stock />} />
+        <Route path="/stocks" element={<Dashboard />} />
       </Routes>
-      
     </div>
   );
 }
---
+++ src/components/MyNav.js
@@ -0,0 +1,31 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function MyNav() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link href="/">
+            <div>HOME</div>
+          </Nav.Link>
+        </Nav>
+
+        <Nav className="me-auto">
+          <Nav.Link href="/stocks">
+            <div>STOCKS</div>
+          </Nav.Link>
+        </Nav>
+
+        <Nav className="me-auto">
+          <Nav.Link href="/about">
+            <div>ABOUT</div>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNav;
